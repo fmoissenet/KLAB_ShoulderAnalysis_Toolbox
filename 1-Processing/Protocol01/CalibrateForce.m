09812c8932c621bfef1c0f161dba564a6204bcdb
Florent Moissenet <florent.moissenet@protonmail.com>
2023-11-09 08:17:03 +0100

    Manage issue when no isometric file available

--- 1-Processing/Protocol01/CalibrateForce.m
+++ 1-Processing/Protocol01/CalibrateForce.m
@@ -47,6 +47,7 @@ if ~isempty(temp)
     c3dFiles   = dir('*.c3d');
     trialTypes = {'ISOMETRIC1','ISOMETRIC2'};
     k          = 1;
+    Trial      = [];
     for i = 1:size(c3dFiles,1)
         for j = 1:size(trialTypes,2)
             if contains(c3dFiles(i).name,trialTypes{j})    
@@ -61,21 +62,23 @@ if ~isempty(temp)
         end
     end
     % For each file
-    for itrial = 1:size(Trial,2)
-        % Get analogs
-        Analog = btkGetAnalogs(Trial(itrial).btk);
-        % Apply force calibration
-        Force = (Analog.FORCE-force.zero) * force.calibration; % N
-        % Update file
-        n     = size(Force,1);
-        k0    = (1:n)';
-        k1    = (linspace(1,n,Trial(itrial).n1))';
-        value = interp1(k0,[Force,zeros(size(Force)),zeros(size(Force))],k1,'spline');
-        try
-            btkSetPoint(Trial(itrial).btk,'Force',value,zeros(length(value),1),'Force signal (N)');
-        catch ME
-            btkAppendPoint(Trial(itrial).btk,'scalar','Force',value,zeros(length(value),1),'Force signal (N)');
+    if ~isempty(Trial)
+        for itrial = 1:size(Trial,2)
+            % Get analogs
+            Analog = btkGetAnalogs(Trial(itrial).btk);
+            % Apply force calibration
+            Force = (Analog.FORCE-force.zero) * force.calibration; % N
+            % Update file
+            n     = size(Force,1);
+            k0    = (1:n)';
+            k1    = (linspace(1,n,Trial(itrial).n1))';
+            value = interp1(k0,[Force,zeros(size(Force)),zeros(size(Force))],k1,'spline');
+            try
+                btkSetPoint(Trial(itrial).btk,'Force',value,zeros(length(value),1),'Force signal (N)');
+            catch ME
+                btkAppendPoint(Trial(itrial).btk,'scalar','Force',value,zeros(length(value),1),'Force signal (N)');
+            end
+            btkWriteAcquisition(Trial(itrial).btk,Trial(itrial).file);
         end
-        btkWriteAcquisition(Trial(itrial).btk,Trial(itrial).file);
     end
 end
